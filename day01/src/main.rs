8418c9ad10488ea3eee1698888648606921e6de9
greg <gregorwa@gmail.com>
2023-12-16 09:01:55 +0100

    commit test

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,27 +1,53 @@
+use std::fs::File;
+use std::io::{self, BufWriter, Write};
+
 mod input_fetcher;
 
-fn count_characters_per_line(input_text: &str) -> Vec<usize> {
+fn process_line(input_text: &str) -> Vec<u32> {
     input_text
         .lines()
-        .map(|line| line.chars().count())
+        .map(|line| {
+            let line_numbers: Vec<u32> = line
+                .chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect();
+
+            if let Some(first) = line_numbers.first() {
+                let last = *line_numbers.last().unwrap_or(&first);
+                format!("{}{}", first, last).parse().unwrap_or(0)
+            } else {
+                0
+            }
+        })
         .collect()
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let day_to_fetch = 1; // Specify the day number you want to fetch
 
     if let Some(input) = input_fetcher::fetch_input(day_to_fetch) {
-        println!("Fetched input data for Day {}:", day_to_fetch);
-        // println!("{}", input);
+        let concatenated_numbers = process_line(&input);
 
-        let character_counts = count_characters_per_line(&input);
+        let output_path = format!("target/concatenated_numbers_day_{}.txt", day_to_fetch);
+        let mut output_file = BufWriter::new(File::create(&output_path)?);
 
-        for (line_number, &count) in character_counts.iter().enumerate() {
-            println!("Line {}: {} characters", line_number + 1, count);
+        let mut sum = 0;
+        for number in concatenated_numbers.iter() {
+            let line_output = format!("{}\n", number);
+            output_file.write_all(line_output.as_bytes())?;
+            sum += number;
         }
-        // Continue working with the fetched input data or character counts here
+
+        let sum_output_path = format!("target/sum_concatenated_numbers_day_{}.txt", day_to_fetch);
+        let mut sum_output_file = BufWriter::new(File::create(sum_output_path)?);
+        sum_output_file.write_all(sum.to_string().as_bytes())?;
+
+        println!("Concatenated numbers per line saved to 'target/concatenated_numbers_day_{}.txt'", day_to_fetch);
+        println!("Sum of concatenated numbers saved to 'target/sum_concatenated_numbers_day_{}.txt'", day_to_fetch);
     } else {
         eprintln!("Error: Failed to fetch input data for Day {}", day_to_fetch);
         // Handle the error here if needed for the specified day
     }
+
+    Ok(())
 }
